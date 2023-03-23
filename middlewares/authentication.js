15868ba826653eaeadb258d58f8629b4ea145a13
Amrutanshu Sahoo <sahooamrutanshu7@gmail.com>
2023-03-23 10:30:41 +0530

    middleware for handling authentication before any request

---
+++ middlewares/authentication.js
@@ -0,0 +1,24 @@
+const {UnauthenticatedError} = require('../errors/unauthenticated')
+const axios = require('axios')
+
+
+// setting up the middleware
+const auth = async (req, res, next) => {
+    // check header of the incoming request
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new UnauthenticatedError('Authentication Invalid')
+    }
+
+    const token = authHeader.split(' ')[1]
+
+    try {
+        // req.user = {id_token: token, authorised: true}
+        next()
+    } catch (error) {
+        throw new UnauthenticatedError('Authentication Invalid')
+    }
+
+}
+
+module.exports = auth
